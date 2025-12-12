3ccdddd17345ff53e3b3c9e2a51288f158a990dc
Alex-Clau <alexgafita47@gmail.com>
2025-12-12 15:17:09 +0200

    feat: Infrastructure setup (PostgreSQL, Express, Docker Compose) as docker image and db connection established.

---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+const pg = require('pg');
+require('dotenv')
+  .config();
+
+const pool = new pg.Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+// design pattern to efficiently manage connection to the db
+// client static connection whilst pool is dynamic that allows multiple connections async
+
+async function main() {
+  const client = await pool.connect();
+
+  try {
+    const response = await client.query('SELECT * FROM subscriber');
+    const {rows} = response;
+    console.log(rows);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.release();
+  }
+}
+
+main()
+  .then(() => console.log('Connected to Postgres!'))
+  .catch(err => console.error('Error connecting to Postgres!', err));
